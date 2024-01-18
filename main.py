87eb8615a0e6d6798dae81aef1f1185804893373
Turganbayev Nauryzbek <smiling.cornflower@gmail.com>
2024-01-18 19:17:02 +0600

    fixed the name of the variable (start)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sart, end = int(input()), int(input())
-numbers = [i for i in range(sart, end + 1)]
+start, end = int(input()), int(input())
+numbers = [i for i in range(start, end + 1)]
 print(*numbers)
 print('some change')
+print(start * end)
