9cbf132e138997c343997ae49e0a0b6959f783e1
Turganbayev Nauryzbek <smiling.cornflower@gmail.com>
2024-01-19 13:24:52 +0600

    TSIS1 and examples from 0 to 20

---
+++ Example_10.py
@@ -0,0 +1,5 @@
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
+else:
+    print("Yes, 'expensive' is present")
---
+++ TSIS1/test.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+
+for i in range(21):
+    file_name = 'Example_' + str(i) + '.py'
+    new_dir = 'TSIS1\\' + file_name
+    shutil.move(file_name, )    
+    
